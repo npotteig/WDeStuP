ac80c3cc4e41af6291c92a43a72d291d7b8b1954
npotteig <npottei1@umbc.edu>
2021-12-05 19:58:36 -0600

    JointJs Bug Issue

--- config/config.default.js
+++ config/config.default.js
@@ -9,5 +9,8 @@ config.mongo.uri = `mongodb://${mongoHost}:27017/webgme_dcrypps`;
 config.plugin.allowServerExecution = true;
 
 
+config.requirejsPaths['jointjs'] = './node_modules/jointjs/dist/joint.min';
+config.requirejsPaths['lodash'] = './node_modules/lodash/lodash.min';
+
 validateConfig(config);
 module.exports = config;
