139317c9270193e7897c822adeb673cabff99ad9
npotteig <npottei1@umbc.edu>
2021-12-08 00:22:17 -0600

    Added visualizer implementation & SVG icons

--- config/config.default.js
+++ config/config.default.js
@@ -8,9 +8,13 @@ const mongoHost = process.env.MONGO_HOST || '127.0.0.1';
 config.mongo.uri = `mongodb://${mongoHost}:27017/webgme_dcrypps`;
 config.plugin.allowServerExecution = true;
 
+config.seedProjects.defaultProject = 'PetriNet';
 
 config.requirejsPaths['jointjs'] = './node_modules/jointjs/dist/joint.min';
 config.requirejsPaths['lodash'] = './node_modules/lodash/lodash.min';
+config.requirejsPaths['jointjscss'] = './node_modules/jointjs/dist/joint';
+config.requirejsPaths['dagrejs'] = './node_modules/dagre/dist/dagre.min';
+config.requirejsPaths['graphlibjs'] = './node_modules/graphlib/dist/graphlib.min';
 
 validateConfig(config);
 module.exports = config;
---
+++ src/plugins/PetriNetVerify/PetriNetVerify/__init__.py
@@ -0,0 +1,125 @@
+"""
+This is where the implementation of the plugin code goes.
+The PetriNetVerify-class is imported from both run_plugin.py and run_debug.py
+"""
+import sys
+import logging
+from webgme_bindings import PluginBase
+
+# Setup a logger
+logger = logging.getLogger('PetriNetVerify')
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)  # By default it logs to stderr..
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+class PetriNetVerify(PluginBase):
+    def main(self):
+        active_node = self.active_node
+        core = self.core
+        logger = self.logger
+        META = self.META
+        
+        transition_graph = {}
+        connected_graph = {}
+        place_graph = {}
+        transitions = {}
+        places = {}
+        arcs = {}
+        
+        
+        nodes = core.load_sub_tree(active_node)
+        for node in nodes:
+            if core.is_instance_of(node, META['Transition']):
+                transitions[core.get_path(node)] = node
+                connected_graph[core.get_path(node)] = []
+                transition_graph[core.get_path(node)] = {'inp':[], 'outp':[]}
+            elif core.is_instance_of(node, META['Arc']):
+                arcs[core.get_path(node)] = node
+            elif core.is_instance_of(node, META['Place']):
+                places[core.get_path(node)] = node
+                connected_graph[core.get_path(node)] = []
+                place_graph[core.get_path(node)] = {'int':[], 'outt':[]}
+        
+        for arc in arcs.keys():
+            myArc = arcs[arc]
+            if core.is_instance_of(myArc, META['InArc']):
+                transition_graph[core.get_pointer_path(myArc, 'dst')]['inp'].append(core.get_pointer_path(myArc, 'src'))
+                place_graph[core.get_pointer_path(myArc, 'src')]['outt'].append(core.get_pointer_path(myArc, 'dst'))
+            elif core.is_instance_of(myArc, META['OutArc']):
+                transition_graph[core.get_pointer_path(myArc, 'src')]['outp'].append(core.get_pointer_path(myArc, 'dst'))
+                place_graph[core.get_pointer_path(myArc, 'dst')]['int'].append(core.get_pointer_path(myArc, 'src'))
+            connected_graph[core.get_pointer_path(myArc, 'src')].append(core.get_pointer_path(myArc, 'dst'))
+            
+        
+        def path_search(root, path, path_len, pathlist):
+            if root is None:
+                return
+            
+            if len(path) > path_len:
+                path[path_len] = root
+            else:
+                path.append(root)
+            
+            path_len += 1
+            if len(connected_graph[root]) == 0:
+                pathlist.append([i for i in path])
+            else:
+                for neighbor in connected_graph[root]:
+                    path_search(neighbor, path, path_len, pathlist)
+        
+        
+        bad_free = False
+        for t in transition_graph.keys():
+            for j in transition_graph.keys():
+                if t != j and len(list(set(transition_graph[t]['inp']) & set(transition_graph[j]['inp']))) > 0:
+                    bad_free = True
+            
+        bad_state = False
+        for t in transition_graph.keys():
+            if len(transition_graph[t]['inp']) != 1 or len(transition_graph[t]['outp']) != 1:
+                bad_state = True
+            
+        bad_marked = False
+        source = None
+        srcct = 0
+        dest = None
+        dstct = 0
+        for p in place_graph.keys():
+            if len(place_graph[p]['int']) != 1 or len(place_graph[p]['outt']) != 1:
+                bad_marked = True
+            if len(place_graph[p]['int']) == 0:
+                source = p
+                srcct += 1
+            if len(place_graph[p]['outt']) == 0:
+                dest = p
+                dstct += 1
+        
+        bad_work = False
+        if source is None or srcct != 1 or dest is None or dstct != 1:
+            bad_work = True
+        else:
+            pths = []
+            path_search(source, [], 0, pths)
+            for pth in pths:
+                if pth[-1] != dest:
+                    bad_work = True
+            for p in place_graph.keys():
+                bad_place = True
+                for pth in pths:
+                    if p in pth:
+                        bad_place = False
+                if bad_place:
+                    bad_work = True
+            
+        if not bad_free:
+            self.send_notification("Your Petri Net is a Free-choice petri net")
+        if not bad_state:
+            self.send_notification("Your Petri Net is a State Machine")
+        if not bad_marked:
+            self.send_notification("Your Petri Net is a Marked Graph")
+        if not bad_work:
+            self.send_notification("Your Petri Net is a Workflow Net")
